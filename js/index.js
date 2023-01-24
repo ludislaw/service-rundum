dce109153d3520b90f972c1a6f8b550221d7f7df
ludislaw <vladderevo2004@gmail.com>
2023-01-24 19:05:36 +0100

    Bugs with height fixed. Adaptation development started

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
 const hiddenGalleryImages = document.querySelectorAll(".hidden");
 const seeMoreButton = document.querySelector(".gallery-seemore");
+const galleryForHeight = document.querySelector(".gallery");
+console.log(galleryForHeight.clientHeight);
+
+let posTop = 0;
 
 seeMoreButton.addEventListener("click", () => {
     hiddenGalleryImages.forEach((element) => {
         console.log(element.scrollHeight);
         if (element.style.maxHeight){
-            window.scrollTo(0, 1900);
-            gallery.style.height = 90 + "vh";
             element.style.maxHeight = null;
-            gallery.style.padding = 50 + "px" + " " + 0 + "px";
             seeMoreButton.style.transform = "rotate(-45deg)";
             seeMoreButton.style.borderColor = "#0f70b7";
+            window.location.hash="gallery";
         } else {
-            window.scrollTo(0, 2600);
-            gallery.style.height = "auto";
-            gallery.style.padding = 70 + "px" + " " + 0 + "px";
+            posTop = window.pageYOffset;
+            window.scrollTo(0, posTop + galleryForHeight.clientHeight + galleryForHeight.clientHeight*0.25);
+            window.location.hash="none";
             element.style.maxHeight = element.scrollHeight + "px";
             seeMoreButton.style.transform = "rotate(-225deg)";
             seeMoreButton.style.borderColor = "#cc1e27";
