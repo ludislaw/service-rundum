18bb64eb14ee3da4f367e1acbf4043ba473ad4ae
ludislaw <vladderevo2004@gmail.com>
2023-02-06 20:19:55 +0100

    Bug with word in 2-nd block fixed

--- send_mail.php
+++ send_mail.php
@@ -1,17 +1,47 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $fname = $_POST['fname'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
+$sender = 'jovifa5349@brandoza.com';
+$recipient = 'jovifa5349@brandoza.com';
 
-  $to = "vladderevo2004@gmail.com";
-  $subject = "New Message from $fname";
-  $headers = "From: $email\r\n";
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
 
-  if (mail($to, $subject, $message, $headers)) {
-    echo "Message sent successfully!";
-  } else {
-    echo "Error sending message.";
-  }
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
 }
+else
+{
+    echo "Error: Message not accepted";
+}
+// function debug_to_console($data) {
+//   $output = $data;
+//   if (is_array($output))
+//       $output = implode(',', $output);
+
+//   echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+// }
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//   $fname = $_POST['fname'];
+//   $email = $_POST['email'];
+//   $message = $_POST['message'];
+
+//   $to = "jovifa5349@brandoza.com";
+//   $subject = "New Message from $fname";
+//   $headers = "From: $email\r\n";
+
+//   debug_to_console($fname);
+//   debug_to_console($email);
+//   debug_to_console($message);
+//   debug_to_console($to);
+//   debug_to_console($subject);
+//   debug_to_console($headers);
+//   debug_to_console(mail($to, $subject, $message, $headers));
+  
+//   if (mail($to, $subject, $message, $headers)) {
+//     echo "Message sent successfully!";
+//   } else {
+//     echo "Error sending message.";
+//   }
+// }
 ?>
